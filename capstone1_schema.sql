cec099b470df449b70d129cadc19f575429b711b
Danielle Regan <regandanielle54@gmail.com>
2023-01-20 07:28:10 -0700

    Add files via upload
    Further updated tables and relationships

--- capstone1_schema.sql
+++ capstone1_schema.sql
@@ -14,13 +14,6 @@ CREATE TABLE breeds
 );
 
 
-CREATE TABLE characteristics 
-(
-    id SERIAL PRIMARY KEY,
-    title TEXT, -->  (max_height_male, max_height_female, min_height_male,min_height_female, max_weight_male, max_weight_female, min_weight_male, min_weight_female, coat_length, shedding, drooling, exercise, barking, good_with_children, good_with_dogs, good_with_strangers)
-)
-
-
 CREATE TABLE users
 (
     id SERIAL PRIMARY KEY, 
@@ -34,6 +27,13 @@ CREATE TABLE users
 );  
 
 
+
+CREATE TABLE characteristics 
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT, -->  (max_height_male, max_height_female, min_height_male,min_height_female, max_weight_male, max_weight_female, min_weight_male, min_weight_female, coat_length, shedding, drooling, exercise, barking, good_with_children, good_with_dogs, good_with_strangers)
+)
+
 CREATE TABLE reviews 
 (
     id SERIAL PRIMARY KEY REFERENCES reviews,
@@ -41,8 +41,8 @@ CREATE TABLE reviews
     behavior_rating INTEGER,
     intelligence_rating INTEGER,
     comments TEXT,
-    breed_id INTEGER REFERENCES breeds,
-    user_id INTEGER REFERENCES users
+    breed_id INTEGER REFERENCES breeds, --> FOREIGN KEY, many-to-many
+    user_id INTEGER REFERENCES users  --> FOREIGN KEY, one-to-many
 );
 
 
@@ -50,25 +50,16 @@ CREATE TABLE dogs_in_home
 (
     id SERIAL PRIMARY KEY,
     photo TEXT,
-    breed_id INTEGER REFERENCES breeds,
-    user_id INTEGER REFERENCES users
+    breed_id INTEGER REFERENCES breeds, --> FOREIGN KEY, many-to-many
+    user_id INTEGER REFERENCES users --> FOREIGN KEY, one-to-many
 )
 
 
 CREATE TABLE breeds_considering
 (
     id SERIAL PRIMARY KEY,
-    breed_id INTEGER REFERENCES breeds,
-    user_id INTEGER REFERENCES users
-)
-
-
-CREATE TABLE users_valued_characteristics
-(
-    id SERIAL PRIMARY KEY,
-    value_rating INTEGER  --> (scale of 1-5, 1 being least, 5 being most)
-    breed_id INTEGER REFERENCES breeds,
-    user_id INTEGER REFERENCES users
+    breed_id INTEGER REFERENCES breeds, --> FOREIGN KEY, many-to-many
+    user_id INTEGER REFERENCES users, --> FOREIGN KEY, one-to-many
 )
 
 
@@ -76,6 +67,17 @@ CREATE TABLE breed_charateristics
 (
     id SERIAL PRIMARY KEY,
     value_rating INTEGER,  --> (scale of 1-5, 1 being least, 5 being most)
-    breed_id INTEGER REFERENCES breeds,
+    breed_id INTEGER REFERENCES breeds, --> FOREIGN KEY, many-to-many
+    characteristic_id INTEGER REFERENCES characteristics --> FOREIGN KEY, many-to-many
 )
+
+
+CREATE TABLE users_valued_characteristics
+(
+    id SERIAL PRIMARY KEY,
+    value_rating INTEGER  --> (scale of 1-5, 1 being least, 5 being most)
+    user_id INTEGER REFERENCES users, --> FOREIGN KEY, many-to-many
+    characteristic_id INTEGER REFERENCES characteristics --> FOREIGN KEY, many-to-many
+)
+
 
